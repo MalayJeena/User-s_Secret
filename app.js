83a4ae2ea4e9f07e44e2aa83fb79369fc53822dd
Malay jeena <malay.jeena998@gmail.com>
2023-04-10 02:47:35 +0530

    raw commit of cookies & Session

--- app.js
+++ app.js
@@ -7,11 +7,99 @@ const salting = 12;
 
 const prisma = new PrismaClient();
 
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+
+
+app.use(session({
+    secret: "Our secret key.",
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+passport.use(new LocalStrategy({
+
+    usernameField: "email",
+    passwordField: "password"
+
+}, async (email, password, done) => {
+    try {
+        
+        const user = await prisma.User.findUnique({
+            where: {
+                email: email
+            }
+        });
+
+        if (!user) {
+            return done(null, false, { message: "User not found" });
+          }
+
+        // compare the stored hashed password with the user's input password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            console.log("whyyyyyyyyyyyyyy")
+            return done(null, user);
+        } else {
+            return done(null, false, {message: "incorrect password"});
+        }
+
+    } catch (error) {
+        
+        console.error(error);
+        
+        return done(error);
+    }
+}));
+
+
+// serializing the cookies
+passport.serializeUser((user, done) => {
+    console.log("User serialized: ", user);
+    done(null, user.id);
+})
+
+
+// deserializing the cookies
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await prisma.User.findUnique({
+            where : {
+                id: id
+            }
+        });
+
+        console.log("User deserialized: ", user);
+        done(null, user);
+
+    } catch (error) {
+        
+        console.error(error);
+        
+        done(error);
+    }
+});
+
+
+
+
 
 
 // render our home page
@@ -30,61 +118,64 @@ app.get("/register", (req, res) => {
 });
 
 
+// render our secrets page
+app.get("/secrets", (req, res) => {
+    console.log('Is authenticated:', req.isAuthenticated());
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    }else {
+        res.redirect("/login");
+    }
+});
+
 
 // post register
 app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+  
     try {
-        const {username, password} = req.body;
-        await bcrypt.hash(password, salting, async function (err, hash){    // added salting in our hashing
-            const newUser = await prisma.User.create({
-                data: {
-                    email: username,
-                    password: hash    // Replaces password with hashed password
-                }
-            });
-        }); 
-        
-        res.status(201).render("secrets");
-        
-    } catch (error) {
-        if (error.code === "InvalidInputError") {
-            res.status(400).json({message: "Invalid input" + error.message});
-        } else {
-            console.log(error);
-            res.status(500).json({message: "And error occurred while creating the user"});
+      const hashedPassword = await bcrypt.hash(password, salting); // hashing and salting user's password
+  
+      const existingUser = await prisma.User.findUnique({
+        where: {
+          email: username
         }
+      });
+  
+      if (existingUser) {
+        // If a user with the same email already exists, send an error response
+        res.status(400).json({ message: "User already exists" });
+        return;
+      }
+  
+      const newUser = await prisma.User.create({
+        data: {
+          email: username,
+          password: hashedPassword, // replaces user's password with hashed password
+        },
+      });
+  
+      res.status(200).redirect("/secrets");
+    } catch (error) {
+      if (error.code === "InvalidInputError") {
+        res.status(400).json({ message: "Invalid input" + error.message });
+      } else {
+        console.log(error);
+        res.status(500).json({ message: "Error occurred while creating the user" });
+        // res.redirect("/register");  // redirecting the user to the register page
+      }
     }
-});
+  });
+
+
 
 
 
 // post login
-app.post("/login", async (req,res) => {
-    try {
-        const {username, password} = req.body;
-        const foundUser = await prisma.User.findUnique({
-            where: {
-                email: username
-            }
-        });
-
-        if (foundUser) {
-            await bcrypt.compare(password, foundUser.password, function (err, result){      // Comparing hashed password with user's input
-                if (result === true) {
-                    res.status(200).render("secrets")
-                } else {
-                    res.status(400).json("message: Password is wrong")
-                }
-            }); 
-            
-        } else {
-            res.status(404).json("message: User not found!!");
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json("error while getting the user");
-    }
-});
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  }));
 
 
 
